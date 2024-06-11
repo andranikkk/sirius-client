5d09793d45de47ef56ee43441b605c3bde054d68
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2024-06-11 19:52:22 +0400

    undo

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
 import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://sirius-server.onrender.com/api",
+  baseUrl: "http://localhost:8000/api",
   prepareHeaders: (headers, { getState }) => {
     const token =
       (getState() as RootState).auth.user?.token ||
