0f896e8f772ad509f058c918723e5536bfa9e851
Andranik Aivazyan <ayvazyanandranik46@gmail.com>
2024-06-11 14:00:06 +0400

    deploy

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react"
 import type { RootState } from "../store"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8000/api",
+  baseUrl: "https://sirius-server.onrender.com/api",
   prepareHeaders: (headers, { getState }) => {
     const token =
       (getState() as RootState).auth.user?.token ||
